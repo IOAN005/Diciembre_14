e5f5e36bf132f759cb8fe413fd17df2c25224c82
IOAN005 <nellbovinca@gmail.com>
2023-12-14 19:52:21 +0100

    clases1

--- src/com/corenetworks/persistencia/AcessoSocios.java
+++ src/com/corenetworks/persistencia/AcessoSocios.java
@@ -1,2 +1,24 @@
-package com.corenetworks.persistencia;public class AcessoSocios {
+package com.corenetworks.persistencia;
+
+import com.corenetworks.modelo.Socio;
+
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class AcessoSocios extends Conexion {
+    public boolean alta(Socio s) throws SQLException, ClassNotFoundException {
+        //1.declarar variable
+        Statement sentencia;
+        int resultado=0;
+        String sql ="insert into socios(carnet,dni) values ('"+s.getCarnet()+"','"+s.getDni()+"');";
+        //2Abrir conexion
+        abrirConexion();
+        //3.Crear el statment
+        sentencia=miConexion.createStatement();
+        //4.Actualizar
+        resultado=sentencia.executeUpdate(sql);
+        //5.Devolver el resultado
+        return resultado>0;
+
+    }
 }
--- src/com/corenetworks/presentacion/ProbarSocio.java
+++ src/com/corenetworks/presentacion/ProbarSocio.java
@@ -1,2 +1,22 @@
-package com.corenetworks.presentacion;public class ProbarSocio {
-}
+package com.corenetworks.presentacion;
+
+import com.corenetworks.modelo.Socio;
+import com.corenetworks.persistencia.AcessoSocios;
+
+import java.sql.SQLException;
+
+public class ProbarSocio {
+    public static void main(String[] args) {
+        AcessoSocios as = new AcessoSocios();
+        Socio s = new Socio("1234567A", "123456");
+
+            try {
+                System.out.println("se ha insertado socio ?" + as.alta(s));
+            } catch (SQLException e) {
+                System.out.println(e.toString());
+            } catch (ClassNotFoundException e) {
+                System.out.println(e.toString());
+            }
+
+        }
+    }
